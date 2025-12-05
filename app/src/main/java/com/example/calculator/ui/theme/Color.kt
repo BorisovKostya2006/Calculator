5bd64399ebcd6a1056f20b594321b7d00ddea588
BorisovKostya2006 <kostyaBorisov075@gmail.com>
2025-12-05 19:51:43 +0300

    chose the colors

--- app/src/main/java/com/example/calculator/ui/theme/Color.kt
+++ app/src/main/java/com/example/calculator/ui/theme/Color.kt
@@ -2,10 +2,19 @@ package com.example.calculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Black = Color(0xFF0D0D0D)
+val White = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Green200 = Color(0x80006600)
+val Green500 = Color(0xCC006600)
+val Green300 = Color(0xCC006600)
+
+val Grey100 = Color(0xFFD9D9D9)
+val Grey200 = Color(0xFFA6A6A6)
+val Grey300 = Color(0x802C2E33)
+val Grey400 = Color(0xCC2C2E33)
+val Grey500 = Color(0xFF333333)
+val Grey600 = Color(0xE50D0D0D)
+
+val Blue500 = Color(0xCC084276)
+val Blue300 = Color(0x80084276)
